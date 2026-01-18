b96085ae27bd4ae546fbcd2deffab59f792f76c6
ToniAnton22 <frincuanton@yahoo.com>
2026-01-18 02:05:46 +0200

    modified: added Link for best practice.

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -6,6 +6,7 @@ import { Project, CreateProjectInput } from "@/types/project";
 import ProjectModal from "@/components/ProjectModal";
 import DeleteConfirmModal from "@/components/DeleteConfirmModal";
 import StatusBadge from "@/components/StatusBadge";
+import Link from "next/link";
 
 export default function ProjectDetailsPage() {
   const params = useParams<{ id: string }>();
@@ -97,13 +98,12 @@ export default function ProjectDetailsPage() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
           <div className="flex items-start justify-between gap-4">
             <div>
-              <button
-                type="button"
-                onClick={() => router.push("/dashboard")}
+              <Link
+                href={"/dashboard"}
                 className="text-sm text-gray-500 hover:text-gray-700"
               >
                 ← Back to Projects
-              </button>
+              </Link>
 
               <h1 className="text-2xl font-bold text-gray-900 mt-2">
                 {isLoading
